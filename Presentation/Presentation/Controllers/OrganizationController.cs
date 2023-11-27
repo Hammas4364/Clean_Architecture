b9514d8bf61d16e8740ccbbff69a33b5b375eae6
Hammas Khan <151642146+Hammas4364@users.noreply.github.com>
2023-11-27 21:39:07 +0500

    ##

---
+++ Presentation/Presentation/Controllers/OrganizationController.cs
@@ -0,0 +1,65 @@
+﻿namespace Presentation.Controllers;
+
+using Application.Interfaces.Services;
+using AutoWrapper.Extensions;
+using AutoWrapper.Wrappers;
+using Domain.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Helpers;
+
+public class OrganizationController : ControllerBase
+{
+    private readonly IOrganizationService organizationService;
+
+    public OrganizationController(IOrganizationService organizationService)
+    {
+        this.organizationService = organizationService;
+    }
+
+    [HttpPost]
+    public async Task<ApiResponse> Add(Add_Org_Dto addDTO, CancellationToken cancellationToken = new())
+    {
+        if (!ModelState.IsValid)
+            throw new ApiException(ModelState.AllErrors());
+        var serviceResult = await organizationService.AddAsync(addDTO, cancellationToken);
+        return serviceResult;
+    }
+
+    [HttpPut]
+    public async Task<ApiResponse> Update(Update_Org_Dto updateDTO, CancellationToken cancellationToken = new())
+    {
+        if (!ModelState.IsValid)
+            throw new ApiException(ModelState.AllErrors());
+        var serviceResult = await organizationService.UpdateAsync(updateDTO, cancellationToken);
+        return serviceResult;
+    }
+
+    [HttpDelete]
+    public async Task<ApiResponse> Remove(Delete_Org_Dto dto, CancellationToken cancellationToken = new())
+    {
+        if (!ModelState.IsValid)
+            throw new ApiException(ModelState.AllErrors());
+        return await organizationService.DeleteAsync(dto, cancellationToken);
+    }
+
+    [HttpGet]
+    public async Task<ApiResponse> GetAll(string? search = default, int? currentPage = default, int? pageSize = default, CancellationToken cancellationToken = new())
+    {
+        if (!ModelState.IsValid)
+            throw new ApiException(ModelState.AllErrors());
+
+        var getAllParams = new GetAllParams(search, currentPage, pageSize);
+        return await organizationService.GetAllAsync(getAllParams, cancellationToken);
+    }
+
+    [HttpGet]
+    [Route("{Id}")]
+    public async Task<ApiResponse> Get(long Id, CancellationToken cancellationToken = new())
+    {
+        if (!ModelState.IsValid)
+            throw new ApiException(ModelState.AllErrors());
+        return await organizationService.GetAsync(Id, cancellationToken);
+    }
+}
--- Presentation/Presentation/obj/Debug/net6.0/Presentation.AssemblyInfo.cs
+++ Presentation/Presentation/obj/Debug/net6.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+322f2b5dd5021fa9e6eba75882cf1739c16ad076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
