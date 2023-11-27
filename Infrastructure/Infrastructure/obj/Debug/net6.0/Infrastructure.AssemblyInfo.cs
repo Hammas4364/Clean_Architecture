8c62d5f2786ff496da4edb276b2a48bd967e737b
Hammas Khan <151642146+Hammas4364@users.noreply.github.com>
2023-11-27 21:34:48 +0500

    ####

--- Infrastructure/Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9d3b592f61b4f03f08275c3fd6255f22c5128e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+322f2b5dd5021fa9e6eba75882cf1739c16ad076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
